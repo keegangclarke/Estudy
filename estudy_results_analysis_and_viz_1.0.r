f7dd40f4d8cf2d87d0f98bbb4d64fb7a1c63a92e
keegangclarke <clarkekeegan@gmail.com>
2022-08-29 13:12:47 +0200

    innit

---
+++ estudy_results_analysis_and_viz_1.0.r
@@ -0,0 +1,8 @@
+# 0. Specify directory strings ####
+
+cd_root <- "C:/Users/Keegan/OneDrive/1 Studies/2021 - 2022/5003W/3 - Dissertation/5-Data/results/estudy/"
+cd_geo <- "geographic_region/"
+cd_icb <- "industry_classication/"
+cd_ar <- "ar/"
+cd_car <- "car/"
+# Use paste0 to get the right names
