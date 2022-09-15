cae023474ca2d33bedc4a7e13291c1a87a28443d
keegangclarke <clarkekeegan@gmail.com>
2022-09-15 16:49:58 +0200

    Imports useful dev functions. Fixed the string components of the database.

--- estudy_results_analysis_and_viz_1.0.r
+++ estudy_results_analysis_and_viz_1.0.r
@@ -1,5 +1,6 @@
 # Load packages
 library(magrittr)
+source("C:/Users/Keegan/Desktop/Repository/@ Development/Estudy_R/development_aid_functions.R")
 
 # CHECK TYPES OF OBJECT
 check_types <- function(object) {
@@ -12,31 +13,40 @@ check_types <- function(object) {
 
 # 0. Specify directory strings and load string info####
 # Directory components
-cd_root <- "C:/Users/Keegan/OneDrive/1 Studies/2021 - 2022/5003W/3 - Dissertation/5-Data/results/estudy/"
-cd_geo <- "geographic_region/"
-cd_icb <- "industry_classication/"
-cd_ar <- "ar/"
-cd_car <- "car/"
-# String-name info
-cd_market_names <- "C:/Users/Keegan/OneDrive/1 Studies/2021 - 2022/5003W/3 - Dissertation/5-Data/results/estudy/geographic_region/names.csv"
+d_root <- "C:/Users/Keegan/OneDrive/1 Studies/2021 - 2022/5003W/3 - Dissertation/5-Data/results/estudy/"
+d_geo <- "geographic_region/"
+d_icb <- "industry_classification/"
+d_ar <- "ar/"
+d_car <- "car/"
+d_id <- "id/"
 
-market_names <- cd_market_names %>% 
-  read.csv(header = TRUE) %>% 
-  unlist %>% 
+# Load id info
+market_names <- paste0(d_root,
+                       d_id,
+                       d_geo,
+                       "market_names.txt") %>% 
+  read.table(header = FALSE) %>% 
+  unlist %>%
   as.vector(mode = "character")
+
+df_sector_id <- paste0(d_root,
+                       d_id,
+                       d_icb,
+                       "sector_data.csv") %>%
+  read.csv(header = TRUE)
 
 # Use paste0 to get the right names
 
 # Load 2 files for testing
-ar_test_file <- read.csv(file = paste0(cd_root,
-                                       cd_geo,
-                                       cd_ar,
+ar_test_file <- read.table(file = paste0(d_root,
+                                       d_geo,
+                                       d_ar,
                                        market_names[[1]],
                                        ".csv")) %>% 
   subset(select = -c(X))
-car_test_file <- read.csv(file = paste0(cd_root,
-                                       cd_geo,
-                                       cd_car,
+car_test_file <- read.csv(file = paste0(d_root,
+                                       d_geo,
+                                       d_car,
                                        market_names[[1]],
                                        ".csv")) %>% 
   subset(select = -c(X))
