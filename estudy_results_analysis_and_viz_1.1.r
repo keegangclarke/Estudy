2ec23f2681958f24a9751902c1f6d36a3213ce8f
keegangclarke <clarkekeegan@gmail.com>
2022-09-22 20:27:14 +0200

    Initial commit. Cleaned up old file and started on clean slate.

---
+++ estudy_results_analysis_and_viz_1.1.r
@@ -0,0 +1,141 @@
+# Load packages & functions
+library(magrittr)
+source("C:/Users/Keegan/Desktop/Repository/@ Development/Estudy_R/development_aid_functions.R")
+
+# 0. Specify directory strings and load string info ####
+# Directory components
+d_root <- "C:/Users/Keegan/OneDrive/1 Studies/2021 - 2022/5003W/3 - Dissertation/5-Data/results/estudy/"
+d_geo <- "geographic_region/"
+d_icb <- "industry_classification/"
+d_res_pres <- "results_presentation/"
+d_id <- "id/"
+
+d_plot <- "plots/"
+d_tables <- "tables/"
+
+d_ar <- "ar/"
+d_aar <- "aar/"
+d_car <- "car/"
+d_caar <- "caar/"
+
+d_proto <- "prototypes/"
+
+# Load id info
+market_names <- paste0(d_root,
+                       d_id,
+                       d_geo,
+                       "market_names.txt") %>% 
+  read.table(header = FALSE) %>% 
+  unlist %>%
+  as.vector(mode = "character")
+
+df_sector_id <- paste0(d_root,
+                       d_id,
+                       d_icb,
+                       "sector_data.csv") %>%
+  read.csv(header = TRUE)
+
+# Load test files
+# PERIODIC ABNORMAL RETURNS
+ar_test_file <- read.csv(file = paste0(d_root,
+                                       d_geo,
+                                       d_ar,
+                                       market_names[[1]],
+                                       ".csv")) %>%
+  as.data.frame()
+colnames(ar_test_file)[[1]] <- 'date'
+ar_test_file[['date']] <- as.Date(ar_test_file[['date']])
+
+aar_test_file <- read.csv(file = paste0(d_root,
+                                        d_geo,
+                                        d_aar,
+                                        market_names[[1]],
+                                        ".csv")) %>%
+  as.data.frame()
+colnames(aar_test_file)[[1]] <- 'date'
+aar_test_file[['date']] <- as.Date(aar_test_file[['date']])
+
+# CUMULATIVE ABNORMAL RETURNS
+car_test_file <- read.csv(file = paste0(d_root,
+                                        d_geo,
+                                        d_car,
+                                        market_names[[1]],
+                                        ".csv")) %>%
+  as.data.frame()
+colnames(car_test_file)[[1]] <- 'date'
+car_test_file[['date']] <- as.Date(car_test_file[['date']])
+
+caar_test_file <- read.csv(file = paste0(d_root,
+                                         d_geo,
+                                         d_caar,
+                                         market_names[[1]],
+                                         ".csv")) %>%
+  as.data.frame()
+colnames(caar_test_file)[[1]] <- 'date'
+caar_test_file[['date']] <- as.Date(caar_test_file[['date']])
+
+# Read in some identifying information
+regions <- readxl::read_xlsx(path = paste0(d_root,
+                                           d_res_pres,
+                                           "table_region_classification_simplified.xlsx")) %>% 
+  as.data.frame()
+
+library(ggplot2)
+
+a_frame <- merge.data.frame(aar_test_file,
+                            caar_test_file,
+                            by = "date") %>%
+  set_names(c('Date', "AAR", "CAAR"))
+
+# retrieving all the caar data ####
+caar_d_geo <- vector(mode = "character",
+                           length = length(market_names))
+for (i in seq_along(caar_d_geo)) {
+  caar_d_geo[[i]] <- paste0(d_root,
+                            d_geo,
+                            d_caar,
+                            market_names[[i]],
+                            ".csv")
+}
+caars_geo <- vector(mode = "list",
+                    length(caar_d_geo))
+
+caars_geo <- fetch_data(caar_d_geo, lst = caars_geo, file_type = "csv") %>%
+  setNames(market_names)
+
+for (i in seq_along(caars_geo)) {
+  names(caars_geo[[i]])[[1]] <- "Date"
+}
+
+
+p5 <- ggplot(data = a_frame) +
+  geom_line(mapping = aes(x = Date,
+                          y = AAR),
+            colour = "navyblue",
+            show.legend = TRUE) +
+  geom_line(mapping = aes(x = Date,
+                          y = CAAR),
+            show.legend = TRUE,
+            colour = "darkred") +
+  geom_hline(yintercept = 0,
+             size = 0.3)+
+  geom_ribbon(#data=subset(x, 2 <= x & x <= 3),
+    aes(x = Date,
+        ymin=AAR,
+        ymax=CAAR),
+    fill="navyblue", alpha=0.1)
+  theme_bw() +
+  labs(title = paste("Average Abnormal Returns:",
+                     names(aar_test_file)[2]),
+  )
+
+ggsave(p5, filename = 'prototype.png',
+       path = paste0(d_root,
+                     d_res_pres,
+                     d_plot,
+                     d_proto),
+       dpi = 300,
+       type = 'cairo',
+       width = 8,
+       height = 4,
+       units = 'in')
