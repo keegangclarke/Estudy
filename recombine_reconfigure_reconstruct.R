da3dd00aafc9f950b163a6e4d1f0a6cfe5298e54
keegangclarke <clarkekeegan@gmail.com>
2022-10-06 10:41:37 +0200

    Initial commit

---
+++ recombine_reconfigure_reconstruct.R
@@ -0,0 +1,8 @@
+# recombine, reconfigure & reconstruct func
+# OBJECTIVE:
+# INPUT: 'list of lists of lists' object, levels are decending in hieracrchy
+# FUNC:   
+      # recombine into 'list of lists', matching item for item on the 3rd level of the object, class('returns')
+      # reconfigure so that constituents are of the smallest length, & params are set to the smallest one
+      # re-list the 3rd level into 'returns' objects
+# OUTPUT: 'list of lists of lists' object
