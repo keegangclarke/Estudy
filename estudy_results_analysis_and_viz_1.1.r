a1b7b5bc897441a7d10f8078828818fffb152e58
keegangclarke <clarkekeegan@gmail.com>
2022-10-07 11:50:41 +0200

    Added variabilisation of events, begun generalisation of existing code, simplified some code, debugging merge_caar_aar which doesn't work always

--- estudy_results_analysis_and_viz_1.1.r
+++ estudy_results_analysis_and_viz_1.1.r
@@ -3,13 +3,90 @@ library(magrittr)
 library(ggplot2)
 source("C:/Users/Keegan/Desktop/Repository/@ Development/Estudy_R/development_aid_functions.R")
 
-# Parameters ####
-event_day <- as.Date("2020-01-22")
+# 1. Parameters ####
+# list to store all event params
+all_events <- make_list(4, 
+                        c("event1", "event2", "event3", "event4"))
+
+event_spec <- function(name = "",
+                       edate = NULL,
+                       bounds = NULL,
+                       est_len = NULL) {
+  if ((name == "") == TRUE) {
+    message("Please specify event name as string.")
+  } else if (is.null(edate) == TRUE) {
+    message("Please specify event date.")
+  } else if ((class(edate) != "Date") == TRUE) {
+    message("Please ensure event date is 'as.Date'")
+  } else if (is.null(bounds) == TRUE) {
+    message("Please specify event bounds.")
+  } else if (is.null(est_len) == TRUE) {
+    message("Please specify estimation length.")
+  } else {
+    event_window <- seq.Date(from = edate - abs(bounds[1]),
+                             to = edate + bounds[2],
+                             by = 'day')
+    estimation_window <-
+      seq.Date(
+        from = edate - abs(bounds[1]) - abs(est_len + 1),
+        to = edate + bounds[1] - 1 ,
+        by = 'day'
+      )
+    
+    event_specification <- vector(mode = 'list', length = 4)
+    event_specification[[1]] <- name
+    event_specification[[2]] <- edate
+    event_specification[[3]] <- event_window
+    event_specification[[4]] <- estimation_window
+    
+    event_specification <-
+      setNames(
+        event_specification,
+        c(
+          "event_name",
+          "event_date",
+          "event_window",
+          "estimation_window"
+        )
+      )
+    
+    class(event_specification) <- "event_spec"
+    return(event_specification)
+  }
+}
+
+all_events[[1]] <- event_spec(
+  "event1",
+  edate = as.Date("2020-01-13"),
+  bounds = c(-5, 5),
+  est_len = 250
+)
+all_events[[2]] <- event_spec(
+  "event2",
+  edate = as.Date("2020-01-24"),
+  bounds = c(-2, 8),
+  est_len = 250
+)
+all_events[[3]] <- event_spec(
+  "event3",
+  edate = as.Date("2020-02-24"),
+  bounds = c(-1, 9),
+  est_len = 250
+)
+all_events[[4]] <- event_spec(
+  "event4",
+  edate = as.Date("2020-03-09"),
+  bounds = c(-1, 9),
+  est_len = 250
+)
 
 # Directory components ####
 d_root <- "C:/Users/Keegan/OneDrive/1 Studies/2021 - 2022/5003W/3 - Dissertation/5-Data/results/estudy/"
-d_geo <- "geographic_region/"
-d_icb <- "industry_classification/"
+
+d_type <- c("geographic_region/","industry_classification/") %>% set_names(c("geo","icb"))
+d_geo <- d_type[["geo"]]
+d_icb <- d_type[["icb"]]
+
 d_res_pres <- "results_presentation/"
 
 d_id <- "id/"
@@ -26,7 +103,8 @@ d_caar <- "caar/"
 
 d_proto <- "prototypes/"
 
-# Load id info ####
+# LOAD ID INFO ####
+# Static since variables not identical format
 geo_fac <- paste0(d_root,
                   d_id,
                   d_geo,
@@ -77,13 +155,11 @@ fetch_rtns <- function(name_lst, directory) {
                      ".csv")
   }
   # create storage list
-  storage_lst <- vector(mode = "list",
-                        length(d))
+  storage_lst <- make_list(length(d), name_lst)
   # retrieve data
   storage_lst <- fetch_data(d,
                             lst = storage_lst,
-                            file_type = "csv") %>%
-    setNames(name_lst)
+                            file_type = "csv")
   # wrangle to be suitable
   for (i in seq_along(storage_lst)) {
     names(storage_lst[[i]])[[1]] <- "Date"
@@ -92,42 +168,67 @@ fetch_rtns <- function(name_lst, directory) {
   }
   return(storage_lst)
 }
+
+## META LOOP L1 ####
+# Cycles through whole process for "geo" and "ICB"
+for (type in 1:2) {
+  TYPE <- d_type[[type]]
+}
+
+# META LOOP L2 ####
+for (i in seq_along(all_events)) {
+  # SPECIFY PARAMS
+  E_NAME <- all_events[[i]]$event_name
+  E_DAY <- all_events[[i]]$event_date
+  E_WIND <- all_events[[i]]$event_window
+  EST_WIND <- all_events[[i]]$estimation_window
+  
+  E_DIR <- paste0(all_events[[i]]$event_name,"/")
+
+}
+
 # ARs & CARs: Retrieve data ####
 # GEOGRAPHIC
-ar_geo <- fetch_ars(name_lst = geo_fac,
+ar_geo <- fetch_rtns(name_lst = geo_fac,
                     directory = paste0(d_root,
                                        d_geo,
+                                       E_DIR,
                                        d_ar))
 # INDUSTRY
-ar_indu <- fetch_ars(name_lst = indu_fac,
+ar_indu <- fetch_rtns(name_lst = indu_fac,
                     directory = paste0(d_root,
                                        d_icb,
                                        d_indu,
+                                       E_DIR,
                                        d_ar))
 # SUPERSECTOR
-ar_supe <- fetch_ars(name_lst = supe_fac,
+ar_supe <- fetch_rtns(name_lst = supe_fac,
                     directory = paste0(d_root,
                                        d_icb,
                                        d_supe,
+                                       E_DIR,
                                        d_ar))
 
 # CUMULATIVE ABNORMAL RETURNS
 # GEOGRAPHIC
-car_geo <- fetch_ars(name_lst = geo_fac,
+car_geo <- fetch_rtns(name_lst = geo_fac,
                     directory = paste0(d_root,
                                        d_geo,
+                                       E_DIR,
                                        d_car))
 # INDUSTRY
-car_indu <- fetch_ars(name_lst = indu_fac,
+car_indu <- fetch_rtns(name_lst = indu_fac,
                      directory = paste0(d_root,
                                         d_icb,
                                         d_indu,
+                                        E_DIR,
                                         d_car))
 # SUPERSECTOR
-car_supe <- fetch_ars(name_lst = supe_fac,
+car_supe <- fetch_rtns(name_lst = supe_fac,
                      directory = paste0(d_root,
                                         d_icb,
                                         d_supe,
+                                        E_DIR,
                                         d_car))
 
 # AARs: RETRIEVE ALL AAR DATA ####
@@ -135,18 +236,21 @@ car_supe <- fetch_ars(name_lst = supe_fac,
 aar_geo <- fetch_rtns(name_lst = geo_fac,
                          directory = paste0(d_root,
                                             d_geo,
+                                            E_DIR,
                                             d_aar))
 # INDUSTRY
 aar_indu <- fetch_rtns(name_lst = indu_fac,
                           directory = paste0(d_root,
                                              d_icb,
                                              d_indu,
+                                             E_DIR,
                                              d_aar))
 # SUPERSECTOR
 aar_supe <- fetch_rtns(name_lst = supe_fac,
                           directory = paste0(d_root,
                                              d_icb,
                                              d_supe,
+                                             E_DIR,
                                              d_aar))
 
 # CAARs: RETRIEVE ALL CAAR DATA ####
@@ -154,18 +258,21 @@ aar_supe <- fetch_rtns(name_lst = supe_fac,
 caar_geo <- fetch_rtns(name_lst = geo_fac,
                         directory = paste0(d_root,
                                            d_geo,
+                                           E_DIR,
                                            d_caar))
 # INDUSTRY
 caar_indu <- fetch_rtns(name_lst = indu_fac,
                          directory = paste0(d_root,
                                             d_icb,
                                             d_indu,
+                                            E_DIR,
                                             d_caar))
 # SUPERSECTOR
 caar_supe <- fetch_rtns(name_lst = supe_fac,
                          directory = paste0(d_root,
                                             d_icb,
                                             d_supe,
+                                            E_DIR,
                                             d_caar))
 
 # FUNCTION TO MERGE CAAR & AAR DATA LISTS TOGETHER FOR PLOTS ####
@@ -174,52 +281,76 @@ merge_caar_aar <-
            name_lst,
            aar_lst,
            caar_lst,
-           evt_day = event_day) {
+           evt_day = NULL) {
     # "storage_lst" == empty list of correct length to store merged data.frames
     # "name_lst" == list of names of groupings
     # "aar_lst" & "caar_lst" == lists of AARs and CAARs
     # "evt_day" == event day by which 'Event.Time' will be calculated
-    for (i in seq_along(name_lst)) {
-      nam <- name_lst[[i]]
-      
-      storage_lst[[nam]] <- merge.data.frame(aar_lst[[nam]],
-                                           caar_lst[[nam]],
-                                           by = "Date") %>%
-        set_names(c('Date', "AAR", "CAAR"))
-      # adds event time column but calculates on logical basis an int vector which sets 0 to 'event_day'
-      # negative ints for days before 'event_day'
-      # postive ints for days after 'event_day'
-      storage_lst[[nam]]$Event.Time <-
-        -nrow(aar_lst[[nam]][(aar_lst[[nam]][["Date"]] < evt_day),]):nrow(aar_lst[[nam]][(aar_lst[[nam]][["Date"]] > evt_day),])
+    if (is.null(evt_day)) {
+      message("No event day specified. Please specify an event day in standard format.")
+    } else {
+      for (i in seq_along(name_lst)) {
+        name <- name_lst[[i]]
+        
+        storage_lst[[name]] <- merge.data.frame(aar_lst[[name]],
+                                               caar_lst[[name]],
+                                               by = "Date") %>%
+          set_names(c('Date', "AAR", "CAAR"))
+        # adds event time column but calculates on logical basis an int vector which sets 0 to 'E_DAY'
+        # negative ints for days before 'E_DAY'
+        # postive ints for days after 'E_DAY'
+        storage_lst[[name]]$Event.Time <-
+          -nrow(aar_lst[[name]][(aar_lst[[name]][["Date"]] < evt_day), ]):nrow(aar_lst[[name]][(aar_lst[[name]][["Date"]] > evt_day), ])
+      }
+      return(storage_lst)
     }
     return(storage_lst)
   }
 
+for (i in seq_along(name_lst)) {
+  print(i)
+  nam <- name_lst[[i]]
+  
+  storage_lst[[nam]] <- merge.data.frame(aar_lst[[nam]],
+                                         caar_lst[[nam]],
+                                         by = "Date") %>%
+    set_names(c('Date', "AAR", "CAAR"))
+  # adds event time column but calculates on logical basis an int vector which sets 0 to 'E_DAY'
+  # negative ints for days before 'E_DAY'
+  # postive ints for days after 'E_DAY'
+  storage_lst[[nam]]$Event.Time <-
+    -nrow(aar_lst[[nam]][(aar_lst[[nam]][["Date"]] < evt_day), ]):nrow(aar_lst[[nam]][(aar_lst[[nam]][["Date"]] > evt_day), ])
+}
+
+
 # Merge AAR & CAAR data.frames ####
 # GEOGRAPHIC
-ave_lst_geo <-
-  vector(mode = "list", length(geo_fac)) %>% set_names(geo_fac)
-ave_lst_geo <- merge_caar_aar(storage_lst = ave_lst_geo,
-                               name_lst = geo_fac,
-                               aar_lst = aar_geo,
-                               caar_lst = caar_geo)
+ave_lst_geo <- make_list(length(geo_fac), geo_fac)
+ave_lst_geo <- merge_caar_aar(
+  storage_lst = ave_lst_geo,
+  name_lst = geo_fac,
+  aar_lst = aar_geo,
+  caar_lst = caar_geo,
+  evt_day = E_DAY
+)
 # INDUSTRY
-ave_lst_indu <- vector(mode = "list",
-                       length(indu_fac)) %>%
-  set_names(indu_fac)
-ave_lst_indu <- merge_caar_aar(storage_lst = ave_lst_indu,
-                               name_lst = indu_fac,
-                               aar_lst = aar_indu,
-                               caar_lst = caar_indu)
+ave_lst_indu <- make_list(length(indu_fac), indu_fac)
+ave_lst_indu <- merge_caar_aar(
+  storage_lst = ave_lst_indu,
+  name_lst = indu_fac,
+  aar_lst = aar_indu,
+  caar_lst = caar_indu,
+  evt_day = E_DAY
+)
 # SUPERSECTOR
-ave_lst_supe <- vector(mode = "list",
-                       length(supe_fac)) %>%
-  set_names(supe_fac)
-ave_lst_supe <- merge_caar_aar(storage_lst = ave_lst_supe,
-                               name_lst = supe_fac,
-                               aar_lst = aar_supe,
-                               caar_lst = caar_supe)
-
+ave_lst_supe <- make_list(length(supe_fac), supe_fac)
+ave_lst_supe <- merge_caar_aar(
+  storage_lst = ave_lst_supe,
+  name_lst = supe_fac,
+  aar_lst = aar_supe,
+  caar_lst = caar_supe,
+  evt_day = E_DAY
+)
 
 # PLOT AARs V CAARs
 aar_caar_plot <- function(name_lst, aar_caar_df_lst, PATH = NULL) {
@@ -279,6 +410,7 @@ aar_caar_plot(
   PATH = paste0(d_root,
                 d_res_pres,
                 d_plot,
+                E_DIR,
                 d_geo,
                 "aar_caar/")
 )
